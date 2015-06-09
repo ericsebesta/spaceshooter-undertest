65bd17913ea65d64e7c93ada34674c3547f3e302
Eric Sebesta <esebesta@gmail.com>
2015-06-09 00:13:54 -0600

    Update install.sh

--- travis-scripts/install.sh
+++ travis-scripts/install.sh
@@ -2,8 +2,8 @@
  
 # Example install script for Unity3D project. See the entire example: https://github.com/JonathanPorta/ci-build
  
-echo 'Downloading from http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg'
-curl -O http://netstorage.unity3d.com/unity/5b98b70ebeb9/MacEditorInstaller/Unity.pkg
+echo 'Downloading from http://netstorage.unity3d.com/unity/0b02744d4013/MacEditorInstaller/Unity-5.0.2f1.pkg'
+curl -O http://netstorage.unity3d.com/unity/0b02744d4013/MacEditorInstaller/Unity-5.0.2f1.pkg
  
-echo 'Installing Unity.pkg'
-sudo installer -dumplog -package Unity.pkg -target /
+echo 'Installing Unity-5.0.2f1.pkg'
+sudo installer -dumplog -package Unity-5.0.2f1.pkg -target /
