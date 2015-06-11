9b382aac838951a8f2fdc8ff32529249c8665eb6
Eric Sebesta <esebesta@gmail.com>
2015-06-11 00:24:35 -0600

    test build return codes to fail travis on build fail

--- scripts-osx/travis-ci/build.sh
+++ scripts-osx/travis-ci/build.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 project="SpaceShooter-undertest"
- 
+
+echo '=========================================================================='
 echo "Attempting to build $project for Windows"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -11,7 +12,13 @@ echo "Attempting to build $project for Windows"
   -projectPath "$(pwd)" \
   -buildWindowsPlayer "$(pwd)/Build/$project-win/$project.exe" \
   -quit
- 
+
+[ $? -eq 0 ] || exit $?; # exit for non-zero return code
+
+echo 'Logs from Windows build'
+cat $(pwd)/unity.log
+
+echo '=========================================================================='
 echo "Attempting to build $project for OS X"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -21,7 +28,13 @@ echo "Attempting to build $project for OS X"
   -projectPath "$(pwd)" \
   -buildOSXUniversalPlayer "$(pwd)/Build/$project-osx/$project.app" \
   -quit
- 
+
+[ $? -eq 0 ] || exit $?; # exit for non-zero return code
+
+echo 'Logs from OS X build'
+cat $(pwd)/unity.log
+
+echo '=========================================================================='
 echo "Attempting to build $project for Linux"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
@@ -31,12 +44,14 @@ echo "Attempting to build $project for Linux"
   -projectPath "$(pwd)" \
   -buildLinuxUniversalPlayer "$(pwd)/Build/$project-lin/$project" \
   -quit
- 
-echo 'Logs from build'
+
+[ $? -eq 0 ] || exit $?; # exit for non-zero return code
+
+echo 'Logs from Linux build'
 cat $(pwd)/unity.log
 
+echo '=========================================================================='
 echo 'Packing the build files to zip files'
-
 echo 'Generating README and VERSION files...'
 cp README.md Build/$project-win/README.md
 cp README.md Build/$project-lin/README.md
@@ -50,12 +65,11 @@ cd Build
 echo 'Removing debug files...'
 rm ./$project-win/*.pdb
 
+echo '=========================================================================='
 echo 'Packing Windows build...'
 zip -r $project-win.zip ./$project-win
-
 echo 'Packing Linux build...'
 zip -r $project-lin.zip ./$project-lin
-
 echo 'Packing OS X build...'
 zip -r $project-osx.zip ./$project-osx
 
