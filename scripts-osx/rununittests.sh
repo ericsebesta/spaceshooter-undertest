531ebb9ed1b0b1e8000efc82e0fea31038fb0f0b
Eric Sebesta <esebesta@gmail.com>
2015-06-12 03:11:15 -0600

    add logging to unit test script

--- scripts-osx/rununittests.sh
+++ scripts-osx/rununittests.sh
@@ -1,10 +1,14 @@
 #! /bin/sh
 
-echo "Running unit tests"
+echo 'Running unit tests'
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-  -projectPath "$(pwd)" \
   -batchmode \
+  -logFile $(pwd)/unity.log \
+  -projectPath "$(pwd)" \
   -executeMethod UnityTest.Batch.RunUnitTests \
   -resultFilePath=results.xml \
   -quit
-
+rc=$?
+echo 'Logs from unit tests'
+cat $(pwd)/unity.log
+if [[ $rc != 0 ]]; then exit $rc; fi
